f9da9be14148a32b79dabc5474ba7363981ddb16
jkbanham <54941260+jkbanham@users.noreply.github.com>
2025-10-29 11:32:27 -0400

    Initialize Azure configuration in config.py
    Added configuration variables for Azure services.

---
+++ config.py
@@ -0,0 +1,5 @@
+# These are unique to the student - should be changed accordingly
+
+az_endpoint = "<replace with your endpoint url>/"
+key = "<replace with your Azure AI Vision key>"  
+storage_connection_string = "<replace with your Azure Storage Blob connection string>"
