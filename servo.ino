5c7c9012950ab8bbd9f37ec77be1691b9598a214
jkbanham <54941260+jkbanham@users.noreply.github.com>
2026-01-25 14:22:47 -0500

    Had the older code in here - updated now
    Added a new servo instance and modified setup and loop functions to read position from serial input.

--- servo.ino
+++ servo.ino
@@ -1,14 +1,19 @@
 #include <Servo.h>
 
+Servo servo1;
+
 Servo servoMotor;
 const int servoPin = 9;
+int pos = 0;
 
 void setup() {
-  servoMotor.attach(servoPin);
-
-  int centerServoAngle = map(-20, -60, 60, 0, 180);
-  servoMotor.write(centerServoAngle);
+  servo1.attach(9);  // Attach servo1 to digital pin 9
+  Serial.begin(9600); // Initialize serial comm at 9600 baud
 }
 
 void loop() {
-}
+  if (Serial.available() > 0) {
+    pos = Serial.read(); // Read the incoming byte (angle)
+    servo1.write(pos);   // Set the servo position
+    }
+  }
